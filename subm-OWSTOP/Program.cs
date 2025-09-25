a7a8d629b44b0d24dcfe76bd0664e20fc1ab00a8
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-09-25 15:10:41 +0100

    menu !

--- subm-OWSTOP/Program.cs
+++ subm-OWSTOP/Program.cs
@@ -1,22 +1,39 @@
 ﻿using static System.Console;
+using static System.Environment;
 using System.Text.RegularExpressions;
 
-namespace subm_OWSTOP {
-    internal class Program {
-        static void Main(string[] args) {
-            Write("enter a number\n% ");
-            WriteLine(range(Convert.ToInt32(ReadLine()!), 6, 14));
-            WriteLine(passcheck(ReadLine()!)); 
+internal class Program {
+    static int Menu() {
+        Write("\x1b[?1049henter a number:\n1: password checking\n2: number boundary checker\n0: quit\n% ");
+        return Convert.ToInt32(ReadLine()!);
+    }
+    static void Main() {
+        switch (Menu()) {
+            case 1:
+                Write(PassCheck(ReadLine()!));
+                break;
+            case 2:
+                Write(Range());
+                break;
+            default:
+                ExitCode = 0;
+                break;
         }
-        static int range(int num, int min, int max) {
-            if (num > min && num < max) return 0;
-            return 1;
-        }
-        static int passcheck(string pass) {
-            Regex reg = new Regex(@"[a-zA-Z]|(\W)");
-            Write("enter a password [MUST BE AT LEAST 10 CHARACTERS AND MUST HAVE BOTH LETTERS AND NON-LETTERS]\n% ");
-            if (pass.Length > 9 && reg.IsMatch(pass)) return 0;
-            return 1;
-        }
+        Write("\x1b[?1049h");
+    }
+    static int Range() {
+        Clear();
+        Write("enter a comma-separated list of numbers [1: num to check, 2: inclusive minimum, 3: inclusive maximum]\n% ");
+        int[] args = Array.ConvertAll(ReadLine()!.Split(","), int.Parse);
+        if (args[0] > args[1] && args[0] < args[2]) return 0;
+        ExitCode = 1;
+        return ExitCode;
+    }
+    static int PassCheck(string pass) {
+        Clear();
+        Regex reg = new Regex(@"[a-zA-Z]|(\W)");
+        Write("enter a password [MUST BE AT LEAST 10 CHARACTERS AND MUST HAVE BOTH LETTERS AND NON-LETTERS]\n% ");
+        if (pass.Length > 9 && reg.IsMatch(pass)) return 0;
+        return 1;
     }
 }
