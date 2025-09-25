73b03cd7095de71ead8b0727465584c192772ad8
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-09-25 14:22:42 +0100

    woa...

--- subm-OWSTOP/Program.cs
+++ subm-OWSTOP/Program.cs
@@ -1,11 +1,12 @@
-﻿using System.Text.RegularExpressions;
+﻿using static System.Console;
+using System.Text.RegularExpressions;
 
 namespace subm_OWSTOP {
     internal class Program {
         static void Main(string[] args) {
-            Console.Write("enter a number\n% ");
-            Console.WriteLine(range(Convert.ToInt32(Console.ReadLine()!), 6, 14));
-            Console.WriteLine(passcheck(Console.ReadLine()!)); 
+            Write("enter a number\n% ");
+            WriteLine(range(Convert.ToInt32(ReadLine()!), 6, 14));
+            WriteLine(passcheck(ReadLine()!)); 
         }
         static int range(int num, int min, int max) {
             if (num > min && num < max) return 0;
@@ -13,7 +14,7 @@ namespace subm_OWSTOP {
         }
         static int passcheck(string pass) {
             Regex reg = new Regex(@"[a-zA-Z]|(\W)");
-            Console.Write("enter a password [MUST BE AT LEAST 10 CHARACTERS AND MUST HAVE BOTH LETTERS AND NON-LETTERS]\n% ");
+            Write("enter a password [MUST BE AT LEAST 10 CHARACTERS AND MUST HAVE BOTH LETTERS AND NON-LETTERS]\n% ");
             if (pass.Length > 9 && reg.IsMatch(pass)) return 0;
             return 1;
         }
